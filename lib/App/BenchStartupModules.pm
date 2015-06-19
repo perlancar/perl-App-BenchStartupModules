d8f7f38539ebde30159a8add8601edffc25e0a2c
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-19 11:54:42 +0700

    Initial commit

---
+++ lib/App/BenchStartupModules.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupModules;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of Perl modules
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-modules>.
